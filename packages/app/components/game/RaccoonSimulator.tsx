1e48c0cb58cdd416322b56ac3ecd4be3e73e9f21
Kristian Gunder Kramås <kristiankramas@outlook.com>
2022-12-23 23:26:37 +0100

    s q u e e z e  🦝🤏

--- packages/app/components/game/RaccoonSimulator.tsx
+++ packages/app/components/game/RaccoonSimulator.tsx
@@ -1,42 +1,129 @@
-import { forwardRef } from "react";
+import { forwardRef, useRef, useState } from "react";
 import Canvas from "../threejs/Canvas";
+import { useFrame } from "../threejs";
 import View, { ViewProps, ViewType } from "../design/View";
-import Raccoon from "./models/Raccoon";
+import RaccoonMesh from "./meshes/Raccoon";
+import { PerspectiveCamera } from "@react-three/drei";
+import { Mesh } from "three";
+import { useSpring } from "@react-spring/three";
 
-export type ThreeProps = {
+export type RaccoonSimulatorProps = {
   //
 } & Omit<Partial<ViewProps>, "children">;
-export type ThreeType = ViewType;
+export type RaccoonSimulatorType = ViewType;
 
-export default forwardRef<ThreeType, ThreeProps>(function Three(
-  { ...props },
-  /**
-   * NB: Ref may have differing types on web and native (your IDE most likely only shows the native types)
-   */
-  ref,
-) {
-  return (
-    <View
-      ref={ref}
-      sx={{
-        flexGrow: 1,
-        width: "100%",
-      }}
-      {...props}
-    >
-      <Canvas
-        // Adjust field of view
-        camera={{
-          fov: 30,
-          // Look down from above at an angle
-          // position: [0, 0, 0],
-          // rotation: [-Math.PI / 4, 0, 0],
+export default forwardRef<RaccoonSimulatorType, RaccoonSimulatorProps>(
+  function RaccoonSimulator({ ...props }, ref) {
+    return (
+      <View
+        ref={ref}
+        sx={{
+          flexGrow: 1,
+          width: "100%",
         }}
+        {...props}
       >
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Raccoon position={[0, 0, 0]} />
-      </Canvas>
-    </View>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <PerspectiveCamera
+            makeDefault
+            position={[0, 5, 10]}
+            rotation={[-0.5, 0, 0]}
+            fov={35}
+            aspect={window.innerWidth / window.innerHeight}
+            near={0.1}
+            far={1000}
+            onUpdate={(self) => self.updateProjectionMatrix()}
+          />
+          <Raccoon />
+        </Canvas>
+      </View>
+    );
+  },
+);
+
+const Raccoon = () => {
+  const mesh = useRef<Mesh>(null);
+  const [squeezed, setSqueezed] = useState(false);
+
+  // Make Raccoon jump every 3 seconds using react-spring
+  const { position, rotation } = useSpring<{
+    position: typeof Mesh.prototype.position;
+    rotation: typeof Mesh.prototype.rotation;
+  }>({
+    from: {
+      position: [0, 0, 0],
+      rotation: [0, 0, 0],
+    },
+    to: async (next) => {
+      while (1) {
+        const oldYRotation = mesh?.current?.rotation.y ?? 0;
+        const random = (Math.random() * Math.PI) / 4 - Math.PI / 8;
+        const newYRotation = oldYRotation + random;
+
+        await next({
+          position: [0, squeezed ? 0.05 : 0.175, 0],
+          rotation: [0, newYRotation, 0],
+          config: {
+            velocity: [0, 0.002, 0],
+            damping: 0.5,
+            clamp: true,
+            tension: 80,
+            friction: 14,
+          },
+          delay: 1000,
+        });
+        await next({
+          position: [0, 0, 0],
+          config: {
+            damping: 0.5,
+            clamp: true,
+            tension: 150,
+            friction: 10,
+          },
+        });
+      }
+    },
+    config: {
+      mass: 1,
+    },
+    onRest: () => {
+      // mesh.current?.scale.set(1, 1, 1);
+      // setSqueezed(false);
+    },
+  });
+
+  // Make Raccoon jump every 3 seconds using react-three-fiber
+  useFrame(() => {
+    if (squeezed) {
+      mesh.current?.scale.set(1.125, 0.825, 1.125);
+      return;
+    }
+    const swell = 0.01;
+    const time = Date.now() / 250;
+    const scale = Math.sin(time) * swell * 0.5 + 1;
+    const oppositeScale = Math.sin(time) * -swell + 1;
+    mesh.current?.scale.set(oppositeScale, scale, oppositeScale);
+  });
+
+  return (
+    <>
+      <RaccoonMesh
+        ref={mesh}
+        position={position}
+        rotation={rotation}
+        onPointerDown={() => {
+          setSqueezed(true);
+          mesh.current?.scale.set(1, 0.5, 1);
+        }}
+        onPointerUp={() => {
+          setSqueezed(false);
+        }}
+        onPointerLeave={() => {
+          setSqueezed(false);
+        }}
+      />
+    </>
   );
-});
+};
--- packages/app/components/game/meshes/Raccoon.tsx
+++ packages/app/components/game/meshes/Raccoon.tsx
@@ -1,40 +1,16 @@
-import { forwardRef, useRef, useEffect, useState } from "react";
-import { useFrame } from "../../threejs";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 import { Mesh } from "three";
-import Cuxel from "./Cuxel";
+import { animated } from "@react-spring/three";
 
-type BoxProps = JSX.IntrinsicElements["mesh"];
+type BoxProps = ComponentPropsWithoutRef<typeof animated.mesh>;
 
 export default forwardRef<Mesh, BoxProps>(function Raccoon(
   props: BoxProps,
   ref,
 ) {
-  const mesh = useRef<Mesh>(null);
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  useFrame(({ clock }) => {
-    // if (mesh.current !== null) mesh.current.rotation.y += 0.01;
-    if (mesh.current !== null)
-      mesh.current.rotation.y = clock.getElapsedTime() * 0.2;
-  });
-
-  useEffect(() => {
-    if (mesh.current !== null) mesh.current.rotation.z = 6;
-    if (mesh.current !== null) mesh.current.rotation.y = 6;
-    if (mesh.current !== null) mesh.current.rotation.x = -6;
-  }, []);
-
   return (
     <>
-      <mesh
-        {...props}
-        ref={ref ?? mesh}
-        scale={active ? 0.9 : 1}
-        onClick={() => setActive(!active)}
-        onPointerOver={() => setHover(true)}
-        onPointerOut={() => setHover(false)}
-      >
+      <animated.mesh {...props} ref={ref}>
         <Cuxel position={[0, -0.1, 0.6]} scale={[1.5, 1, 4]} color="gray" />
         <Cuxel position={[0, -0.15, 0.6]} scale={[3, 1, 1]} color="gray" />
         <Cuxel position={[0, -0.075, 0.8]} scale={[1, 1, 1]} color="#0f0f0f" />
@@ -131,8 +107,26 @@ export default forwardRef<Mesh, BoxProps>(function Raccoon(
           />
         </mesh>
         <boxGeometry args={[1, 0.8, 1]} />
-        <meshStandardMaterial color={"#434353"} opacity={hovered ? 0.8 : 1} />
-      </mesh>
+        <meshStandardMaterial color={"#434353"} />
+      </animated.mesh>
     </>
+  );
+});
+
+type CuxelProps = {
+  size?: number;
+  color?: string;
+} & JSX.IntrinsicElements["mesh"];
+
+const Cuxel = forwardRef<Mesh, CuxelProps>(function Cuxel(
+  { size = 1 / 8, color = "#31313a", children, ...props }: CuxelProps,
+  ref,
+) {
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry args={[size, size, size]} />
+      <meshStandardMaterial color={color} />
+      {children}
+    </mesh>
   );
 });
