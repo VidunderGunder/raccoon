cee356b4a8413c65528b3d3d721ed161c893ca36
Kristian Gunder Kramås <kristiankramas@outlook.com>
2022-12-23 17:17:53 +0100

    He lives

--- packages/app/components/game/models/Raccoon.tsx
+++ packages/app/components/game/models/Raccoon.tsx
@@ -13,11 +13,11 @@ export default forwardRef<Mesh, BoxProps>(function Raccoon(
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
-  // useFrame(({ clock }) => {
-  //   // if (mesh.current !== null) mesh.current.rotation.y += 0.01;
-  //   if (mesh.current !== null)
-  //     mesh.current.rotation.y = clock.getElapsedTime() * 0.2;
-  // });
+  useFrame(({ clock }) => {
+    // if (mesh.current !== null) mesh.current.rotation.y += 0.01;
+    if (mesh.current !== null)
+      mesh.current.rotation.y = clock.getElapsedTime() * 0.2;
+  });
 
   useEffect(() => {
     if (mesh.current !== null) mesh.current.rotation.z = 6;
@@ -37,17 +37,83 @@ export default forwardRef<Mesh, BoxProps>(function Raccoon(
       >
         <Cuxel position={[0, -0.1, 0.6]} scale={[1.5, 1, 4]} color="gray" />
         <Cuxel position={[0, -0.15, 0.6]} scale={[3, 1, 1]} color="gray" />
-        <Cuxel position={[0, -0.075, 0.8]} scale={[1, 1, 1]} color="black" />
-
-        <Cuxel position={[0, +0.05, 0.5]} scale={[7, 4, 0.5]} color="gray" />
+        <Cuxel position={[0, -0.075, 0.8]} scale={[1, 1, 1]} color="#0f0f0f" />
         <Cuxel
-          position={[0, +0.05, 0.5]}
-          scale={[6.5, 3.5, 0.75]}
-          color="black"
+          position={[0, -0.15, 0.8]}
+          scale={[1, 1, 0.25]}
+          rotation={[0, 0, Math.PI / 4]}
+          color="#853164"
         />
 
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={"#31313a"} opacity={hovered ? 0.8 : 1} />
+        <Cuxel
+          position={[+0.175, +0.025, 0.5]}
+          scale={[3, 2, 0.75]}
+          rotation={[0, 0, -0.6]}
+          color="#252529"
+        />
+        <Cuxel
+          position={[-0.175, +0.025, 0.5]}
+          scale={[3, 2, 0.75]}
+          rotation={[0, 0, 0.6]}
+          color="#252529"
+        />
+
+        <Cuxel
+          position={[+0.175, +0.025, 0.51]}
+          scale={[3.75, 2.5, 0.5]}
+          rotation={[0, 0, -0.6]}
+          color="gray"
+        />
+        <Cuxel
+          position={[-0.175, +0.025, 0.51]}
+          scale={[3.75, 2.5, 0.5]}
+          rotation={[0, 0, 0.6]}
+          color="gray"
+        />
+
+        <Cuxel
+          position={[+0.175, +0.025, 0.5]}
+          scale={[0.5, 0.5, 0.85]}
+          color="white"
+        />
+        <Cuxel
+          position={[-0.175, +0.025, 0.5]}
+          scale={[0.5, 0.5, 0.85]}
+          color="white"
+        />
+
+        <Cuxel
+          position={[+0.225, 0.4125, 0.3]}
+          scale={[2.25, 2.25, 0.25]}
+          rotation={[0, 0, Math.PI / 4]}
+          color="#434353"
+        />
+        <Cuxel
+          position={[-0.225, 0.4125, 0.31]}
+          scale={[1.5, 1.5, 0.25]}
+          rotation={[0, 0, Math.PI / 4]}
+          color="#7d4d8b"
+        />
+
+        <Cuxel
+          position={[+0.225, 0.4125, 0.31]}
+          scale={[1.5, 1.5, 0.25]}
+          rotation={[0, 0, Math.PI / 4]}
+          color="#7d4d8b"
+        />
+        <Cuxel
+          position={[-0.225, 0.4125, 0.3]}
+          scale={[2.25, 2.25, 0.25]}
+          rotation={[0, 0, Math.PI / 4]}
+          color="#434353"
+        />
+
+        <Cuxel position={[0, -0.2, -0.55]} scale={[3, 3, 1]} color="#34343d" />
+        <Cuxel position={[0, -0.2, -0.675]} scale={[3, 3, 1]} color="#434353" />
+        <Cuxel position={[0, -0.2, -0.8]} scale={[3, 3, 1]} color="#34343d" />
+
+        <boxGeometry args={[1, 0.8, 1]} />
+        <meshStandardMaterial color={"#434353"} opacity={hovered ? 0.8 : 1} />
       </mesh>
     </>
   );
